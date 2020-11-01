325889fd97ead1fde386d894a71a7bdc7c384665
AgnikaSunil <agnika.sunil@gmail.com>
2020-11-01 12:39:43 +0530

    C71

---
+++ config.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+require('@firebase/firestore');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCbdx--8OIFszG7cAt7bVv1KMpBRQgltvk",
+    authDomain: "c71willyapp.firebaseapp.com",
+    databaseURL: "https://c71willyapp.firebaseio.com",
+    projectId: "c71willyapp",
+    storageBucket: "c71willyapp.appspot.com",
+    messagingSenderId: "377580050497",
+    appId: "1:377580050497:web:23e72336539e51363d0ec0"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
